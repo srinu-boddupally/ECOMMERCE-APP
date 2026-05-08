97d337cb3f8826acfd4714ef988a61efe5a30911
srinu-boddupally <boddupallysrinu76@gmail.com>
2026-05-08 17:20:56 +0530

    Setup backend server configuration and database connection

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+// Route for user login
+const loginUser = async(req,res)=>{
+
+
+}
+
+const registerUser = async(req,res)=>{
+  
+  res.json({msg:"Register API Working"})
+
+}
+
+//Route for admin login
+const adminLogin = async(req,res)=>{
+
+}
+
+
+export {loginUser,registerUser,adminLogin}
+
