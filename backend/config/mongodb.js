c039348bf9c443c115c7d042a2b994e12c2be24d
srinu-boddupally <boddupallysrinu76@gmail.com>
2026-05-09 16:34:36 +0530

    feat: implement user authentication and product upload with Cloudinary

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async() =>{
+const connectDB = async () => {
 
-   mongoose.connection.on('connected',() =>{
-     console.log("DB Connected");
-   })
+  mongoose.connection.on('connected', () => {
+    console.log("DB Connected");
+  })
 
-   await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce}`)
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 
 export default connectDB;
