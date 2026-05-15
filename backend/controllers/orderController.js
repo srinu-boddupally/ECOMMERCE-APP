24ac3745695591aa5c5c54f5679b2b3462e5c81c
srinu-boddupally <boddupallysrinu76@gmail.com>
2026-05-15 11:59:47 +0530

    feat: display dynamic user orders data from backend API

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,29 +5,29 @@ import userModel from "../models/userModel.js";
 const placeOrder = async (req, res) => {
 
   try {
-   
-   const {userId, items, amount, address} = req.body;
 
-   const orderData ={
-    userId,
-    items,
-    address,
-    amount,
-    paymentMethod:"COD",
-    payment:false,
-    date: Date.now()
-   }
-     
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now()
+    }
+
     const newOrder = new orderModel(orderData)
     await newOrder.save()
 
-    await userModel.findByIdAndUpdate(userId,{cartData:{}})
-    
-     res.json({success:true,message:"Order placed"})
+    await userModel.findByIdAndUpdate(userId, { cartData: {} })
+
+    res.json({ success: true, message: "Order placed" })
 
   } catch (error) {
-   console.log(error);
-   res.json({success:false,message:error.message})
+    console.log(error);
+    res.json({ success: false, message: error.message })
   }
 
 }
@@ -45,11 +45,32 @@ const placeOrderRazorpay = async (req, res) => {
 // All Orders data for Admin panel
 const allOrders = async (req, res) => {
 
+  try {
+
+    const orders = await orderModel.find({})
+    res.json({ success: true, orders })
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+  }
+
+
 }
 
 // User Order Data For Frontend
 const userOrders = async (req, res) => {
+  try {
 
+    const { userId } = req.body
+
+    const orders = await orderModel.find({ userId })
+    res.json({ success: true, orders })
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+  }
 }
 
 
